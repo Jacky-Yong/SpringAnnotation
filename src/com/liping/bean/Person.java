5c732e1bbf08c94f8fabc843720d0a139a7a9f41
LIP <LIP@163.com>
2022-07-10 23:55:38 +0800

    first commit

---
+++ src/com/liping/bean/Person.java
@@ -0,0 +1,66 @@
+package com.liping.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class Person {
+
+    @Value("李平")
+    private String name;
+
+    //SpEl
+    //@Value("#{18+5}")
+    //读取配置文件中的key和value
+    @Value("${person.age}")
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Perosn{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    /**
+     * 以下两个注解属于java注解的规范
+     */
+    //对象创建并赋值后调用
+    @PostConstruct
+    public void initPerson(){
+        System.out.println("@PostConstruct..");
+    }
+
+    //容器移除对象之前调用
+    @PreDestroy
+    public void destroyPerson(){
+        System.out.println("@PreDestroy...");
+    }
+
+}
